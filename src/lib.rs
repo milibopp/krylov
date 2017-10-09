0e283a899ad6c3162d8056ccdf5ce2d0846ecdce
Eduard Bopp <eduard.bopp@aepsil0n.de>
2017-10-09 17:17:15 +0200

    Represent matrix as a function

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate nalgebra;
 #[macro_use]
 extern crate approx;
 
-use nalgebra::{Vector3, Matrix3, dot, norm, zero};
+use nalgebra::{Vector3, dot, norm, zero};
 
 #[derive(Clone, Copy)]
 struct State {
@@ -16,15 +16,18 @@ struct State {
     v: Vector3<f64>
 }
 
-pub fn bicgstab(guess: Vector3<f64>, vector: Vector3<f64>, matrix: Matrix3<f64>) -> Vector3<f64> {
-    let initial = initial_state(guess, &vector, &matrix);
-    let good_enough = |value: &Vector3<f64>| norm(&(matrix * value - vector)) < 0.01;
+pub fn bicgstab<F>(guess: Vector3<f64>, vector: Vector3<f64>, matrix: F) -> Vector3<f64>
+    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+{
+    let matrix = &matrix;
+    let initial = initial_state(guess, &vector, matrix);
+    let good_enough = |value: &Vector3<f64>| norm(&(matrix(value) - vector)) < 0.01;
     let steps = (0..2).cycle()
         .map(|i| move |state|
             if i == 0 {
-                first_step(state, &matrix, &initial.difference)
+                first_step(state, matrix, &initial.difference)
             } else {
-                second_step(state, &matrix)
+                second_step(state, matrix)
             }
         );
 
@@ -34,31 +37,32 @@ pub fn bicgstab(guess: Vector3<f64>, vector: Vector3<f64>, matrix: Matrix3<f64>)
         .unwrap()
 }
 
-fn initial_state(guess: Vector3<f64>, vector: &Vector3<f64>, matrix: &Matrix3<f64>) -> State {
+fn initial_state<F>(guess: Vector3<f64>, vector: &Vector3<f64>, matrix: F) -> State
+    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+{
     State {
-        value: guess,
-        difference: vector - matrix * &guess,
-        rho: 1.,
-        omega: 1.,
-        alpha: 1.,
-        p: zero(),
-        v: zero()
+        value: guess, difference: vector - matrix(&guess),
+        rho: 1., omega: 1., alpha: 1., p: zero(), v: zero()
     }
 }
 
-fn first_step(state: State, matrix: &Matrix3<f64>, reference_diff: &Vector3<f64>) -> State {
+fn first_step<F>(state: State, matrix: F, reference_diff: &Vector3<f64>) -> State
+    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+{
     let rho = dot(reference_diff, &state.difference);
     let beta = (rho / state.rho) * (state.alpha / state.omega);
     let p = state.difference + beta * (state.p - state.omega * state.v);
-    let v = matrix * p;
+    let v = matrix(&p);
     let alpha = rho / dot(reference_diff, &v);
     let value = state.value + alpha * p;
     State { value, rho, p, v, alpha, ..state }
 }
 
-fn second_step(state: State, matrix: &Matrix3<f64>) -> State {
+fn second_step<F>(state: State, matrix: F) -> State
+    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+{
     let s = state.difference - state.alpha * state.v;
-    let t = matrix * s;
+    let t = matrix(&s);
     let omega = dot(&t, &s) / dot(&t, &t);
     let value = state.value + state.omega * s;
     let difference = s - state.omega * t;
@@ -81,7 +85,7 @@ mod tests {
         let solution = Vector3::new(2., -1., -2.);
         let vector = matrix * solution;
         let guess = Vector3::new(1., 0., 1.);
-        let result = bicgstab(guess, vector, matrix);
+        let result = bicgstab(guess, vector, |v| matrix * v);
         assert_relative_eq!(result, solution, max_relative = 1e-4);
     }
 }
