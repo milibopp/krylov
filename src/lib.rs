0fe3424be7d000fea920b663b006cc263271df81
Eduard Bopp <eduard.bopp@aepsil0n.de>
2017-10-09 18:52:00 +0200

    Abstract over type of vector space

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,95 @@
 extern crate nalgebra;
+extern crate alga;
 #[cfg(test)]
 #[macro_use]
 extern crate approx;
 
-use nalgebra::{Vector3, dot, norm, zero};
+use nalgebra::{dot, norm, zero};
+use alga::linear::{FiniteDimVectorSpace, VectorSpace, NormedSpace};
+use alga::general::{Identity, Multiplicative};
 
 #[derive(Clone, Copy)]
-struct State {
-    value: Vector3<f64>,
-    difference: Vector3<f64>,
-    rho: f64,
-    omega: f64,
-    alpha: f64,
-    p: Vector3<f64>,
-    v: Vector3<f64>
+struct State<V: VectorSpace> {
+    value: V,
+    difference: V,
+    rho: V::Field,
+    omega: V::Field,
+    alpha: V::Field,
+    p: V,
+    v: V
 }
 
-pub fn bicgstab<F>(guess: Vector3<f64>, vector: Vector3<f64>, matrix: F) -> Vector3<f64>
-    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+pub fn bicgstab<V, F>(guess: V, vector: V, matrix: F, tolerance: V::Field) -> V
+    where V: FiniteDimVectorSpace + NormedSpace + Clone,
+          V::Field: Identity<Multiplicative> + PartialOrd + Clone,
+          F: Fn(&V) -> V,
 {
     let matrix = &matrix;
-    let initial = initial_state(guess, &vector, matrix);
-    let good_enough = |value: &Vector3<f64>| norm(&(matrix(value) - vector)) < 0.01;
+    let initial = initial_state(guess, vector.clone(), matrix);
+    let good_enough = |value: &_| norm(&(matrix(value) - vector.clone())) < tolerance;
+    let initial_difference = &initial.difference;
     let steps = (0..2).cycle()
         .map(|i| move |state|
             if i == 0 {
-                first_step(state, matrix, &initial.difference)
+                first_step(state, matrix, &initial_difference)
             } else {
                 second_step(state, matrix)
             }
         );
 
-    steps.scan(initial, |state, step| { *state = step(*state); Some(*state) })
+    steps.scan(initial.clone(), |state, step| { *state = step(state.clone()); Some(state.clone()) })
         .find(|state| good_enough(&state.value))
         .map(|state| state.value)
         .unwrap()
 }
 
-fn initial_state<F>(guess: Vector3<f64>, vector: &Vector3<f64>, matrix: F) -> State
-    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+fn initial_state<V, F>(guess: V, vector: V, matrix: F) -> State<V>
+    where V: VectorSpace,
+          V::Field: Identity<Multiplicative>,
+          F: Fn(&V) -> V
 {
     State {
-        value: guess, difference: vector - matrix(&guess),
-        rho: 1., omega: 1., alpha: 1., p: zero(), v: zero()
+        difference: vector - matrix(&guess),
+        value: guess,
+        rho: Identity::identity(),
+        omega: Identity::identity(),
+        alpha: Identity::identity(),
+        p: zero(),
+        v: zero()
     }
 }
 
-fn first_step<F>(state: State, matrix: F, reference_diff: &Vector3<f64>) -> State
-    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+fn first_step<V, F>(state: State<V>, matrix: F, reference_diff: &V) -> State<V>
+    where V: FiniteDimVectorSpace + Clone,
+          V::Field: Clone,
+          F: Fn(&V) -> V
 {
     let rho = dot(reference_diff, &state.difference);
-    let beta = (rho / state.rho) * (state.alpha / state.omega);
-    let p = state.difference + beta * (state.p - state.omega * state.v);
+    let beta = (rho.clone() / state.rho) * (state.alpha / state.omega.clone());
+    let p = state.difference.clone() + (state.p - state.v * state.omega.clone()) * beta;
     let v = matrix(&p);
-    let alpha = rho / dot(reference_diff, &v);
-    let value = state.value + alpha * p;
+    let alpha = rho.clone() / dot(reference_diff, &v);
+    let value = state.value + p.clone() * alpha.clone();
     State { value, rho, p, v, alpha, ..state }
 }
 
-fn second_step<F>(state: State, matrix: F) -> State
-    where F: Fn(&Vector3<f64>) -> Vector3<f64>
+fn second_step<V, F>(state: State<V>, matrix: F) -> State<V>
+    where V: FiniteDimVectorSpace + Clone,
+          V::Field: Clone,
+          F: Fn(&V) -> V
 {
-    let s = state.difference - state.alpha * state.v;
+    let s = state.difference - state.v.clone() * state.alpha.clone();
     let t = matrix(&s);
     let omega = dot(&t, &s) / dot(&t, &t);
-    let value = state.value + state.omega * s;
-    let difference = s - state.omega * t;
+    let value = state.value + s.clone() * state.omega.clone();
+    let difference = s - t * state.omega;
     State { value, omega, difference, ..state }
 }
 
 
 #[cfg(test)]
 mod tests {
-    use nalgebra::{Matrix3, Vector3};
+    use nalgebra::{Matrix3, Vector3, Matrix4, Vector4};
     use super::*;
 
     #[test]
@@ -85,7 +102,17 @@ mod tests {
         let solution = Vector3::new(2., -1., -2.);
         let vector = matrix * solution;
         let guess = Vector3::new(1., 0., 1.);
-        let result = bicgstab(guess, vector, |v| matrix * v);
+        let result = bicgstab(guess, vector, |v| matrix * v, 1e-8);
+        assert_relative_eq!(result, solution, max_relative = 1e-4);
+    }
+
+    #[test]
+    fn solves_differently_sized_system() {
+        let matrix = Matrix4::new_random();
+        let solution = Vector4::new_random();
+        let vector = matrix * solution;
+        let guess = Vector4::new_random();
+        let result = bicgstab(guess, vector, |v| matrix * v, 1e-8);
         assert_relative_eq!(result, solution, max_relative = 1e-4);
     }
 }
